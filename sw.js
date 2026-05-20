695577800dc9acfb1f4b521c56529406e1897dd2
ygormachado-dev <llibidpower@gmail.com>
2026-05-20 18:52:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-const CACHE = 'estoque-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Service Worker minimo - nao intercepta chamadas de API
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
+// Nao faz cache de nada - deixa tudo passar direto
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
+ 
